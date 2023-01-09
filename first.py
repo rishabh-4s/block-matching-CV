5b25157b1607dfffc97b94ad6789316962ccb020
Rishabh Sukhija <sukhijarishabh44@gmail.com>
2023-01-09 13:20:23 +0530

    hi

---
+++ first.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+imgL = cv.imread('tsukuba_l.png', 0)
+imgR = cv.imread('tsukuba_r.png',0)
+# imgL = cv.cvtColor(imgL, cv.COLOR_BGR2GRAY)
+# imgR = cv.cvtColor(imgR, cv.COLOR_BGR2GRAY)
+stereo = cv.StereoBM_create(numDisparities=16, blockSize=15)
+disparity = stereo.compute(imgL,imgR)
+# plt.imshow(disparity)
+plt.imshow(disparity,'gray')
+plt.axis('off')
+plt.show()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+imgL = cv.cvtColor(imgL, cv.COLOR_BGR2GRAY)
+# imgR = cv.cvtColor(imgR, cv.COLOR_BGR2GRAY)
